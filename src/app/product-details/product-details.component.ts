b935ad1d7fa25251cab4c52a9de756070bff9b91
Aaron Murphy <aaron.murphy@capco.com>
2022-01-21 10:30:11 -0500

    Revert "Added shopping cart and shipping prices."
    This reverts commit a8c6e465c416f30a58cf974b8eedc32fc6b912c8.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products} from '../products';
-import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,10 +11,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(
-      private route: ActivatedRoute,
-      private cartService: CartService
-    ) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     // Get product id from route
@@ -24,11 +20,6 @@ export class ProductDetailsComponent implements OnInit {
 
     // Find product from id
     this.product = products.find(product => product.id === productIdFromRoute);
-  }
-
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
   }
 
 }
